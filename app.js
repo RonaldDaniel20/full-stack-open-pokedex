4e10305be0d167e40af6ebbfb4cb3ddede5690d9
RonaldDaniel20 <rjacanamejoy@unal.edu.co>
2025-12-16 17:54:35 -0500

    Añade test para pepeline v1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('The server is healthy test')
+  res.send('The server is healthy test v1')
 })
 
 app.listen(PORT, () => {
